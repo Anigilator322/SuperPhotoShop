d72112574ce43d17e74790fe9d8ee353da792d09
Anigilator322 <Anigilator322@gmail.com>
2024-10-26 22:20:17 +0700

    Init 1.0

---
+++ SuperPhotoShop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SuperPhotoShop.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+        #region Title
+        private string _title = "SuperPhohotoshop2";
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        } 
+        #endregion
+    }
+}
